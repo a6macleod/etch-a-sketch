59a80996c67323f0081edef644b0d2f6065f67b5
Andrew MacLeod <A6macleod@gmail.com>
2018-09-02 15:03:27 -0400

    Initial commit add basic HTML and CSS beginnings of JS

---
+++ script.js
@@ -0,0 +1,16 @@
+let sketchArea = 16;
+let color = black;
+let opacity = 10%;
+
+
+function divGrid (sketchArea) {
+	let grid = document.div[#sketchOutline];
+	for (i = 0; i < sketchArea; i++) {
+		let row = document.createElement("div");
+		row.className = "row";
+		for (i=0; i< sketchArea; i++) {
+			let column = document.createElement("div");
+			column.className = "column";
+		}
+	}
+}
