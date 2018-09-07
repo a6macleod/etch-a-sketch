4229dea9658a6bf1fdd20f390e48520384c0ad05
Andrew MacLeod <A6macleod@gmail.com>
2018-09-07 11:55:59 -0400

    Add working single color sketch

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 let sketchArea = 16;
-//let color = black;
-//let opacity = 10%;
+let gridColor = "black";
+let colorOpacity = 0;
 
 
 function divGrid (v) {
-	let e = document.getElementById("sketchOutline");
+	let e = document.querySelector("#sketchOutline");
 	for (i = 0; i < v; i++) {
 		for (x = 1; x <= v; x++) {
 			let cell = document.createElement("div");
 			let gridSize = 600 / sketchArea;
 			console.log(gridSize);
 			cell.setAttribute("class", "cell");
-			cell.setAttribute("style", "width: " + gridSize + "px; height: " + gridSize +"px;");
+			cell.setAttribute("style", "width: " + gridSize + "px; height: " + gridSize +"px; background-color: "
+				+ gridColor + "; opacity: " + colorOpacity + ";");
 			sketchOutline.appendChild(cell);
-//			gridHover(cell);
 //			clearGrid(cell);
 //			newGrid(cell);
+		
+			cell.addEventListener ("mouseover", function (event) {
+				console.log('what?');
+				event.target.style.opacity = (+ 0.5);
+			});
 		}
 	}
 }
@@ -31,7 +36,7 @@ function activateButtons() {
 
 
 // 1) start button changes to reset button
-// 2) Create the initial 16x16 grid (start with having a border)
+// 2) 
 // 3) classic and Colorful buttons turn on from grey
-// 4) Input box opens up to allow the modification of the grid size
+// 4) Reset button prompts up to allow the modification of the grid size
 // 5) reset button clears the colors but not the grid
