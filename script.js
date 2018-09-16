de9c3ed4a612cf2d79c634353c5bbc05b0dbd996
Andrew MacLeod <A6macleod@gmail.com>
2018-09-16 14:15:24 -0400

    Update activateButton method and click functionality to JS file

--- script.js
+++ script.js
@@ -1,72 +1,86 @@
-let sketchArea = 16;
+let sketchArea = 36;
 let gridColor = "black";
 let colorOpacity = 0;
 
 
-function divGrid (v) {
+//let loadClassic = document.querySelector("#classic");
+//	loadClassic.classList.toggle("classicActive");
+//let loadColorful = document.querySelector("#colorful");
+//	loadColorful.classList.toggle("colorfulActive");
+//let loadStart = document.querySelector("#start");
+//	loadStart.classList.toggle("resetActive");
+
+let gridStart = document.querySelector("#start");
+gridStart.addEventListener("click", function () { 
+	console.log("startClicked");
 	let e = document.querySelector("#sketchOutline");
-	for (i = 0; i < v; i++) {
-		for (x = 1; x <= v; x++) {
+	let size = sketchArea
+	for (i = 0; i < size; i++) {
+		for (x = 1; x <= size; x++) {
 			let cell = document.createElement("div");
-			let gridSize = 600 / sketchArea;
-			console.log(gridSize);
+			let cellSize = 600 / size;
+			console.log(cellSize);
 			cell.setAttribute("class", "cell");
-			cell.setAttribute("style", "width: " + gridSize + "px; height: " + gridSize +"px; background-color: "
+			cell.setAttribute("style", "width: " + cellSize + "px; height: " + cellSize +"px; background-color: "
 				+ gridColor + "; opacity: " + colorOpacity + ";");
 			sketchOutline.appendChild(cell);
-//			clearGrid(cell);
-//			newGrid(cell);
-		
 			cell.addEventListener ("mouseover", function (event) {
 				console.log('what?');
 				event.target.style.opacity = parseFloat(event.target.style.opacity) + 0.1;
 			});
 		}
 	}
-}
-
-function activateButtons() {
-	document.querySelector(".classic").setAttribute("class", "classicActive");
-	document.querySelector(".colorful").setAttribute("class", "colorfulActive");
-	document.querySelector(".reset").setAttribute("class", "resetActive");
-	document.querySelector(".start").setAttribute("class", "startInactive");
-}
-
-
-// Adds the RESET to the button
-let re = document.querySelector("#reset");
-re.addEventListener ("click", function (event) {
-	console.log("reset clicked");
-	colorOpacity = 0;
-	let sketchOutline = document.querySelector("#sketchOutline");
-	while (sketchOutline.firstChild) {
-		sketchOutline.removeChild(sketchOutline.firstChild)
-	};
-
-//	resizeGrid();
+	activateButtons();
 });
 
-//function clearOpacity() {
+function activateButtons() {
+	let classic = document.querySelector("#classic");
+		classic.classList.toggle("inactive");
+		classic.classList.toggle("classicActive");
+	let start = document.querySelector("#start");
+		start.classList.toggle("start");
+		start.classList.toggle("resetActive");
+	let colorful = document.querySelector("#colorful");
+		colorful.classList.toggle("inactive");
+		colorful.classList.toggle("colorfulActive");
+//	document.querySelector("#start").setAttribute("class", "startInactive");
+	//let gridStart = 
+	//	gridStart.innerHTML = "Reset";
+}
+
+
+// Adds the RESET functionality to reset button
+//let re = document.querySelector("#reset");
+//re.addEventListener ("click", function (event) {
+//	console.log("reset clicked");
 //	colorOpacity = 0;
-//	let cellClear = document.querySelectorAll(".cell");//.style.opacity = "0";
-//	console.log(cellClear.length);
-//	cell.setAttribute("style", "opacity: " + colorOpacity + ";");
-//}	
-//	let gridUpdate = prompt("Pick a grid size 1-100!", "16");
-//	if (gridUpdate === null || gridUpdate === NaN || gridUpdate < 1 || gridUpdate > 100) {
-//		colorOpacity = 0;
+//	let sketchOutline = document.querySelector("#sketchOutline");
+//	while (sketchOutline.firstChild) {
+//		sketchOutline.removeChild(sketchOutline.firstChild)
+//	};
+//
+//	resizeGrid();
+//});
+//
+//function resizeGrid(){
+//	let gridUpdate = window.prompt("Pick a grid size 1-100!", "16");
+//	let userChoice = parseInt(gridUpdate);
+//	if (userChoice === null || userChoice === NaN || userChoice < 1 || userChoice > 100) {
+////		colorOpacity = 0;
+//
 //		divGrid(16);
 //	} else {
-//		let userChoice = parseFloat(gridUpdate);
-//		divGrid(userChoice);
-
+//		let userChoice = parseInt(gridUpdate);
+//		userChoice = sketchArea;
+//		divGrid(sketchArea);
+//
 //	}
+//}
 
 
-
-// 1) 
-// 2) reset button clears the grid
-// 3) classic and Colorful buttons turn on from grey
-// 4) Reset button prompts up to allow the modification of the grid size
-// 5) 
+// 1) Reset button prompts to allow the modification of the grid size
+// 2) Classic button changes the colors to black/white
+// 3) Colorful button adds random colors 
+// 4) Start button disapears
+// 5) reset button appears after clicking start button
 
